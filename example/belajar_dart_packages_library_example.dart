e9a8ffe15b8e5a94bcb8b9605d9ddb7811b0cde0
JavierGavra <javiergavra01@gmail.com>
2023-09-07 11:05:29 +0700

    dart library

---
+++ example/belajar_dart_packages_library_example.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_packages_library/hello.dart';
+
+void main() {
+  sayHello("Gavra");
+
+  var customer = Customer();
+  print(customer.runtimeType);
+}
---
+++ lib/src/say_hello.dart
@@ -0,0 +1,1 @@
+void sayHello(String name) => print("Hello $name");
